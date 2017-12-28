a9908c5a8eb96176daa2d18559366907029c6009
Iwona Sasak <sasak.iwona.88@gmail.com>
2017-12-28 17:00:29 +0100

    First Classes

---
+++ src/main/java/model/CopyFrase.java
@@ -0,0 +1,27 @@
+package model;
+
+public class CopyFrase {
+	private String name;
+	private String frase;
+
+	public CopyFrase(String name, String frase) {
+		this.name = name;
+		this.frase = frase;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFrase() {
+		return frase;
+	}
+
+	public void setFrase(String frase) {
+		this.frase = frase;
+	}
+}
---
+++ src/main/java/view/CopyButton.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import javax.swing.JButton;
+import model.CopyFrase;
+
+public class CopyButton extends JButton {
+
+	private static final long serialVersionUID = 1L;
+	private CopyFrase copyFrase;
+
+	public CopyButton(CopyFrase copyFrase) {
+		this.copyFrase = copyFrase;
+	}
+	
+	public void copyToClipboard() {
+		StringSelection selection = new StringSelection(copyFrase.getFrase());
+	    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+	    clipboard.setContents(selection, selection);
+	    // TODO Add text to quick edit section
+	}
+
+}
